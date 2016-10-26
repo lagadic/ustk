93607c2ff57a39bd1b376ffebe6fa173d92a2056
marc pouliquen <marc.pouliquen@inria.fr>
2016-10-26 19:05:18 +0200

    first implementation(incomplete) of pixelMeter conversion

--- modules/ustk_core/src/meterConversion/usPixelMeterConversion.cpp
+++ modules/ustk_core/src/meterConversion/usPixelMeterConversion.cpp
@@ -34,6 +34,7 @@
 * @brief Conversion between a position in meters in the space and the equivalent pixel position in the ultrasound image.
 */
 
+#include <algorithm>
 #include <visp3/ustk_core/usPixelMeterConversion.h>
 
 
@@ -42,14 +43,64 @@ void usPixelMeterConversion::convert(const usImagePostScan2D<unsigned char> &ima
                                      double &x,  double &y)
 {
   //checking transducer settings to apply corresponding transformation
-
-
+  //First convex probe type
+  if(image.isTransducerConvex()) {
+    x = image.getWidthResolution() * u - ((image.getWidth()*image.getWidthResolution())/2);
+    y = image.getHeightResolution() * v + image.getTransducerRadius() * std::cos((image.getScanLineNumber()-1)*image.getScanLinePitch()/2);
+  }
+  //Then linear probe type
+  else {
+    x = image.getWidthResolution() * u - ((image.getWidth()*image.getWidthResolution()-1)/2);
+    y = image.getHeightResolution() * v;
+  }
 }
 
+/**
+ * @brief Conversion for 3D post-scan images.
+ * @param image
+ * @param u
+ * @param v
+ * @param w
+ * @param x
+ * @param y
+ * @param z
+ *
+ * @warning Make sure you completed the following transducer settings, the motor settings, and the image settings (element spacing) before the conversion.
+ */
 void usPixelMeterConversion::convert(const usImagePostScan3D<unsigned char> &image,
                                      const double &u, const double &v, const double &w,
                                      double &x,  double &y, double &z)
 {
   //checking transducer/motor settings to apply corresponding transformation
+    if(!image.isTransducerConvex()) { //linear transducer
+      if(image.getMotorType() == usMotorSettings::LinearMotor) { //linear motor
+        x = image.getElementSpacingX() * u - ((image.getDimX()*image.getElementSpacingX())/2);
+        y = image.getElementSpacingY() * v;
+        z = image.getElementSpacingZ() * w - (image.getDimZ()*image.getElementSpacingZ()/2);
+      }
+      else if (image.getMotorType() == usMotorSettings::TiltingMotor) { //tilting motor
+        x = image.getElementSpacingX() * u - ((image.getDimX()*image.getElementSpacingX())/2);
+        y = image.getElementSpacingY() * v + image.getMotorRadius() * std::cos((image.getFrameNumber() - 1) * image.getFramePitch() / 2) - image.getMotorRadius();
+        z = image.getElementSpacingZ() * w - (image.getDimZ()*image.getElementSpacingZ()/2);
+      }
+      else
+        throw(vpException(vpException::notImplementedError, "Rotationnal Motor is not available yet."));
+  }
+  //Then convex transducer
+  else {
+    if(image.getMotorType() == usMotorSettings::LinearMotor) { //linear motor
+      x = image.getElementSpacingX() * u - (image.getDimX() * image.getElementSpacingX() / 2);
+      y = image.getElementSpacingY() * v + image.getTransducerRadius() * std::cos((image.getScanLineNumber() - 1) * image.getScanLinePitch() / 2);
+      z = image.getElementSpacingZ() * w - (image.getDimZ() * image.getElementSpacingZ() / 2);
+    }
+    else if (image.getMotorType() == usMotorSettings::TiltingMotor) { //tilting motor
+      x = image.getElementSpacingX() * u - (image.getDimX() * image.getElementSpacingX() / 2);
+      //TO CORRECT
+      //y = image.getElementSpacingY() * v + image.getTransducerRadius() * std::cos((image.getScanLineNumber()-1)*image.getScanLinePitch()/2);
+      z = image.getElementSpacingZ() * w - (image.getDimZ() * image.getElementSpacingZ() / 2);
 
+    }
+    else
+      throw(vpException(vpException::notImplementedError, "Rotationnal Motor is not available yet."));
+  }
 }
