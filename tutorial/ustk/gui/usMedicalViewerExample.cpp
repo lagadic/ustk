47685e2d39001c71e7e82c4ff2c50d2326d43485
marc pouliquen <marc.pouliquen@inria.fr>
2017-01-10 11:43:26 +0100

    replacing file in viewer

--- tutorial/ustk/gui/usMedicalViewerExample.cpp
+++ tutorial/ustk/gui/usMedicalViewerExample.cpp
@@ -7,7 +7,8 @@ int main( int argc, char** argv )
   // QT application
   QApplication app( argc, argv );
 
-  std::string fileName = "/home/mpouliqu/Documents/ustk-dataset/3D/volumeTest.mhd";
+  //std::string fileName = "/home/mpouliqu/Documents/ustk-dataset/3D/volumeTest.mhd";
+  std::string fileName = "/home/mpouliqu/Documents/ustk-dataset/pre-scan/3D_mhd/volume.mhd";
   usMedicalImageViewer medicalImageViewer(fileName);
   medicalImageViewer.show();
 
