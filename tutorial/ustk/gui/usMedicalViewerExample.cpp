7e410482ab311364f91de88d89f38a7d32f09604
Fabien Spindler <Fabien.Spindler@inria.fr>
2017-01-16 09:24:37 +0100

    Introduce command line options in gui tutorials

--- tutorial/ustk/gui/usMedicalViewerExample.cpp
+++ tutorial/ustk/gui/usMedicalViewerExample.cpp
@@ -5,17 +5,28 @@
 #include <visp3/ustk_core/us.h>
 #include <visp3/ustk_gui/usMedicalImageViewer.h>
 
-int main( int argc, char** argv )
+int main(int argc, char** argv)
 {
   std::string mhd_filename;
 
+  for (int i=0; i<argc; i++) {
+    if (std::string(argv[i]) == "--input")
+      mhd_filename = std::string(argv[i+1]);
+    else if (std::string(argv[i]) == "--help") {
+      std::cout << "\nUsage: " << argv[0] << " [--input <3D volume.mhd>] [--help]\n" << std::endl;
+      return 0;
+    }
+  }
+
   // Get the ustk-dataset package path or USTK_DATASET_PATH environment variable value
-  std::string env_ipath = us::getDataSetPath();
-  if (! env_ipath.empty())
-    mhd_filename = env_ipath + "/pre-scan/3D_mhd/volume.mhd";
-  else {
-    std::cout << "You should set USTK_DATASET_PATH environment var to access to ustk dataset" << std::endl;
-    return 0;
+  if (mhd_filename.empty()) {
+    std::string env_ipath = us::getDataSetPath();
+    if (! env_ipath.empty())
+      mhd_filename = env_ipath + "/pre-scan/3D_mhd/volume.mhd";
+    else {
+      std::cout << "You should set USTK_DATASET_PATH environment var to access to ustk dataset" << std::endl;
+      return 0;
+    }
   }
 
   // QT application
