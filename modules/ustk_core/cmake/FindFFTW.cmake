b95d58f2945ef04130aca4e2fcd82fee48898d62
Fabien Spindler <Fabien.Spindler@inria.fr>
2017-05-18 14:14:32 +0200

    Imprve fftw detection

--- modules/ustk_core/cmake/FindFFTW.cmake
+++ modules/ustk_core/cmake/FindFFTW.cmake
@@ -1,20 +1,19 @@
-# - Find FFTW
-# Find the native FFTW includes and library
+# Find the native fftw includes and libraries
 #
-# FFTW_INCLUDES - where to find fftw3.h
+# FFTW_FOUND - True if fftw found.
+# FFTW_INCLUDE_DIRS - where to find fftw3.h
 # FFTW_LIBRARIES - List of libraries when using FFTW.
-# FFTW_FOUND - True if FFTW found.
 
-if (FFTW_INCLUDES)
+if(FFTW_INCLUDE_DIRS)
   # Already in cache, be silent
-  set (FFTW_FIND_QUIETLY TRUE)
-endif (FFTW_INCLUDES)
+  set(FFTW_FIND_QUIETLY TRUE)
+endif(FFTW_INCLUDE_DIRS)
 
-find_path (FFTW_INCLUDES fftw3.h)
-find_library (FFTW_LIBRARIES NAMES fftw3 fftw3_omp)
+find_path(FFTW_INCLUDE_DIRS fftw3.h)
+find_library(FFTW_LIBRARIES NAMES fftw3 fftw3_omp)
 
 # handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if
 # all listed variables are TRUE
-include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDES)
-mark_as_advanced (FFTW_LIBRARIES FFTW_INCLUDES)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDE_DIRS)
+mark_as_advanced(FFTW_LIBRARIES FFTW_INCLUDE_DIRS)
