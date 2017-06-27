762ac2d1ac782ee9b14744c41cfc7687e7b5650b
Marc Pouliquen <marc.pouliquen@inria.fr>
2017-06-27 15:47:52 +0200

    added documentation for structs of network grabber

--- modules/ustk_grabber/include/visp3/ustk_grabber/usNetworkGrabber.h
+++ modules/ustk_grabber/include/visp3/ustk_grabber/usNetworkGrabber.h
@@ -70,75 +70,93 @@ class VISP_EXPORT usNetworkGrabber : public QObject
 {
   Q_OBJECT
 public:
-#ifndef DOXYGEN_SHOULD_SKIP_THIS
   // Following headers must be the same in the server (ultrasound station) !
 
-  //header sent by the client to init porta
+  /**
+   * header sent by the client to init porta on server side.
+   */
   struct usInitHeaderSent{
     usInitHeaderSent() : headerId(1) {} // set header Id to 1 by default
-    int headerId; //to differenciate usInitHeaderSent (=1) / usAcquisitionParameters (=2) / usRunControlHeaderSent(=3)
+    int headerId; /**< header id to differenciate usInitHeaderSent (=1) / usAcquisitionParameters (=2) / usRunControlHeaderSent(=3) */
 
-    //probe / slot selection
-    int probeId; // available probes : 4DC7 (id = 15) , C5-2 (id = 10)
-    int slotId; // 3 slots on ultrasonix station (0 : top, 2 : middle, 1 : botom)
-
-    int imagingMode; // see ImagingModes.h
+  /**
+   * @name probe / slot selection
+   */
+  /*@{*/
+    int probeId; /**< available probes : 4DC7 (id = 15) , C5-2 (id = 10) */
+    int slotId; /**<  3 slots on ultrasonix station (0 : top, 2 : middle, 1 : botom) */
+  /*@}*/
+    int imagingMode; /**< see ImagingModes.h in porta SDK */
   };
 
-  //header sent by the client to run/stop the acquisition
+  /**
+   * header sent by the client to run/stop the acquisition on server side.
+   */
   struct usRunControlHeaderSent{
     usRunControlHeaderSent() : headerId(3), run(false) {} // set header Id to 1 by default
-    int headerId; //to differenciate usInitHeaderSent (=1) / usAcquisitionParameters (=2) / usRunControlHeaderSent(=3)
+    int headerId; /**< header id to differenciate usInitHeaderSent (=1) / usAcquisitionParameters (=2) / usRunControlHeaderSent(=3) */
 
-    bool run; // run an acquisition if true, stop it if false
+    bool run; /**< boolean to run (true) or stop (false) the aquisition */
   };
 
-  //header sent by the server to confirm porta is initialized
+  /**
+   * header sent by the server to confirm porta is initialized
+   */
   struct usInitHeaderConfirmation{
     usInitHeaderConfirmation() : headerId(1), initOk(0), probeId(0) {} // set header Id to 1 by default
-    int headerId; // Never change this value ! It is used to differenciate usInitHeaderConfirmation (=1) / usImageHeader (=2)
+    int headerId; /**<  Never change this value ! It is used to differenciate usInitHeaderConfirmation (=1) / usImageHeader (=2) */
 
-    int initOk; // 1 if init ok, 0 otherwise
-    int probeId; // unique code for each probe (4DC7 = 15, C 50-62 = 10)
+    int initOk; /**<  1 if init ok, 0 otherwise */
+    int probeId; /**<  unique code for each probe (4DC7 = 15, C 50-62 = 10) */
   };
 
-  // Header coming before every frame
+
+  /**
+   *  Header coming before every frame
+   */
   struct usImageHeader{
     usImageHeader() : headerId(2) {} //set header Id to 2 by default
-    int headerId; // Never change this value ! It is used to differenciate usInitHeaderConfirmation (=1) / usImageHeader (=2)
+    int headerId; /**<  Never change this value ! It is used to differenciate usInitHeaderConfirmation (=1) / usImageHeader (=2) */
 
-    quint32 frameCount; //from the beginning of last acquisition
-    quint64 timeStamp; //msecs since epoch (on ultrasond machine)
+    quint32 frameCount; /**< from the beginning of last acquisition */
+    quint64 timeStamp; /**< msecs since epoch (on ultrasond machine) */
 
-    double dataRate; // in FPS
+    double dataRate; /**< in FPS */
 
-    int dataLength; //frame size in bytes, used to read on the network
-    int ss;	// sample size in bits
+    int dataLength; /**< frame size in bytes, used to read on the network */
+    int ss;	/**< sample size in bits */
 
-    int imageType;	 	// type of data (0 = pre-scan, 1 = post-scan, 2 = rf)
+    int imageType;	 	/**< type of data (0 = pre-scan, 1 = post-scan, 2 = rf) */
 
-    int frameWidth; // width of a frame (pixels for post-scan, scanlines for pre-scan or rf data)
-    int frameHeight; // height of frame (pixels for post-scan, samples for pre-scan or rf data)
+    int frameWidth; /**< width of a frame (pixels for post-scan, scanlines for pre-scan or rf data) */
+    int frameHeight; /**< height of frame (pixels for post-scan, samples for pre-scan or rf data) */
 
-    double pixelWidth; // width of post-scan pixel
-    double pixelHeight; // height of post-scan pixel
+    double pixelWidth; /**< width of post-scan pixel */
+    double pixelHeight; /**< height of post-scan pixel */
 
-    int transmitFrequency; // in Hz
-    int samplingFrequency; // in Hz
+    int transmitFrequency; /**< in Hz */
+    int samplingFrequency; /**< in Hz */
 
-    //transducer settings
-    double transducerRadius;
-    double scanLinePitch;
-    unsigned int scanLineNumber;
-    int imageDepth; //in mm
+  /**
+   * @name transducer settings
+   */
+  /*@{*/
+    double transducerRadius; /**< radius of the probe (for convex ones), in meters (0 if linear)*/
+    double scanLinePitch; /**< Angle (rad) between two scan-lines (if convex)) */
+    unsigned int scanLineNumber; /**< number of scanLines used for acquisition*/
+    int imageDepth; /**< depth of the image, in mm */
+  /*@}*/
 
-    // motor settings
-    int degPerFr; // degree step between frames
-    int framesPerVolume; //number of frames in a volume
+  /**
+   * @name motor settings
+   */
+  /*@{*/
+    int degPerFr; /**< degree step between frames*/
+    int framesPerVolume; /**< number of frames in a volume */
+  /*@}*/
 
 
   };
-#endif //DOXYGEN_SHOULD_SKIP_THIS
 
   explicit usNetworkGrabber(QObject *parent = 0);
   ~usNetworkGrabber();
@@ -151,7 +169,7 @@ public:
 
   void runAcquisition();
 
-  void setIPAddress(std::string s_ip){m_ip = s_ip;}
+  void setIPAddress(const std::string &s_ip){m_ip = s_ip;}
 
   void setActivateMotor(bool activateMotor);
   void setDegreesPerFrame(int degreesPerFrame);
