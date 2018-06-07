cf260e466989ee8100175cb80ff8f98bf8f2ee77
Jason Chevrie <jason.chevrie@irisa.fr>
2018-06-07 22:59:58 +0200

    Add missing licenses

--- modules/ustk_image_processing/ustk_needle_detection/include/visp3/ustk_needle_detection/usPolynomialCurve3D.h
+++ modules/ustk_image_processing/ustk_needle_detection/include/visp3/ustk_needle_detection/usPolynomialCurve3D.h
@@ -25,9 +25,7 @@
  * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
  * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * Authors:
- * Pierre Chatelain
- * Alexandre Krupa
+ * Author:
  * Jason Chevrie
  *
  *****************************************************************************/
