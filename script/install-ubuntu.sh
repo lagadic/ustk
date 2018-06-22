5c85a64bf894f96c37a4539cfcd3df5d474a1b90
Marc Pouliquen <marc.pouliquen@inria.fr>
2018-06-22 11:39:30 +0200

    upadte ubuntu installation script, to resolve armadillo issue on ubuntu versions prior to 18.04

--- script/install-ubuntu.sh
+++ script/install-ubuntu.sh
@@ -3,9 +3,20 @@
 #
 # Usage: sh install-ubuntu.sh
 #
-
 echo "UsTK installation Script"
 echo "Installing dependencies..."
+
+# Check ubuntu version
+UBUNTU_VERSION=$(grep 'VERSION_ID' /etc/os-release | grep -o -E '[0-9]+.[0-9]+')
+# Compare to 18.04, to know if libarmadillo8 package is available
+UBUNTU_VERSION_18_04='18.04'
+LATEST_VERSION="`echo -e "$UBUNTU_VERSION\n$UBUNTU_VERSION_18_04" | sort -V | head -n1`"
+
+if [ "$LATEST_VERSION" = "$UBUNTU_VERSION_18_04" ]; then
+    echo "ustk_elastography module requires armadillo 6.7 or newer (ubuntu package includes too old version of armadillo on ubuntu versions prior to 18.04), so ustk_elastography will not be built."
+else
+    sudo apt-get install libarmadillo8
+fi
 
 sudo apt-get install build-essential
 sudo apt-get install cmake-curses-gui
@@ -39,8 +50,14 @@ else
     cd $USTK_WS/ustk-build
 
     echo "Configuring project with CMake..."
-    cmake $USTK_WS/visp -DVISP_CONTRIB_MODULES_PATH=$USTK_WS/ustk -DBUILD_MODULE_visp_ar=OFF -DBUILD_MODULE_visp_blob=OFF -DBUILD_MODULE_visp_detection=OFF -DBUILD_MODULE_visp_klt=OFF -DBUILD_MODULE_visp_mbt=OFF -DBUILD_MODULE_visp_me=OFF -DBUILD_MODULE_visp_tt=OFF -DBUILD_MODULE_visp_tt_mi=OFF -DBUILD_MODULE_visp_vision=OFF -DBUILD_MODULE_visp_visual_features=OFF -DBUILD_MODULE_visp_vs=OFF
+
+    if [ "$LATEST_VERSION" = "$UBUNTU_VERSION_18_04" ]; then  
+        cmake $USTK_WS/visp -DVISP_CONTRIB_MODULES_PATH=$USTK_WS/ustk -DBUILD_MODULE_visp_ar=OFF -DBUILD_MODULE_visp_blob=OFF -DBUILD_MODULE_visp_detection=OFF -DBUILD_MODULE_visp_klt=OFF -DBUILD_MODULE_visp_mbt=OFF -DBUILD_MODULE_visp_me=OFF -DBUILD_MODULE_visp_tt=OFF -DBUILD_MODULE_visp_tt_mi=OFF -DBUILD_MODULE_visp_vision=OFF -DBUILD_MODULE_visp_visual_features=OFF -DBUILD_MODULE_visp_vs=OFF -DBUILD_MODULE_visp_ustk_elastography=OFF
+    else    
+        cmake $USTK_WS/visp -DVISP_CONTRIB_MODULES_PATH=$USTK_WS/ustk -DBUILD_MODULE_visp_ar=OFF -DBUILD_MODULE_visp_blob=OFF -DBUILD_MODULE_visp_detection=OFF -DBUILD_MODULE_visp_klt=OFF -DBUILD_MODULE_visp_mbt=OFF -DBUILD_MODULE_visp_me=OFF -DBUILD_MODULE_visp_tt=OFF -DBUILD_MODULE_visp_tt_mi=OFF -DBUILD_MODULE_visp_vision=OFF -DBUILD_MODULE_visp_visual_features=OFF -DBUILD_MODULE_visp_vs=OFF
 	 
+    fi
+
     echo "Compiling project"
     make -j4
 
