6563e69aa714bf9b526e0681c4b043676d375d54
Marc Pouliquen <marc.pouliquen@inria.fr>
2018-06-28 16:58:13 +0200

    added command-line options to select slot, probe, and imaging mode in ultrasonix grabber tutorials

--- tutorial/ustk/ultrasonix/tutorial-ultrasonix-qt-grabbing-RF.cpp
+++ tutorial/ustk/ultrasonix/tutorial-ultrasonix-qt-grabbing-RF.cpp
@@ -34,9 +34,23 @@ int main(int argc, char **argv)
 
   // setting acquisition parameters
   usNetworkGrabber::usInitHeaderSent header;
-  header.probeId = 15;     // 4DC7 id = 15
-  header.slotId = 0;       // top slot id = 0
-  header.imagingMode = 12; // B-mode = 0, RF = 12
+  if (qApp->arguments().contains(QString("--probeID"))) {
+        header.probeId = qApp->arguments().at(qApp->arguments().indexOf(QString("--probeID")) + 1).toInt();
+  }
+  else
+    header.probeId = 15;    // 4DC7 id = 15 by default
+
+  if (qApp->arguments().contains(QString("--slotID"))) {
+    header.slotId = qApp->arguments().at(qApp->arguments().indexOf(QString("--slotID")) + 1).toInt();
+  }
+  else
+    header.slotId = 0;      // top slot id = 0 by default
+
+  if (qApp->arguments().contains(QString("--imagingMode"))) {
+        header.imagingMode = qApp->arguments().at(qApp->arguments().indexOf(QString("--imagingMode")) + 1).toInt();
+  }
+  else
+    header.imagingMode = 12; // RF = 12
 
   // prepare image;
   usFrameGrabbedInfo<usImageRF2D<short int> > *grabbedFrame;
